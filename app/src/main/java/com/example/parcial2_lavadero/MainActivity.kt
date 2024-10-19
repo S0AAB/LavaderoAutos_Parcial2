a1ca5161f89557dbecd30221d77849a98cf1222a
s0aab <santiago.ah.ballen@gmail.com>
2024-10-19 14:29:17 -0500

    First commit

---
+++ app/src/main/java/com/example/parcial2_lavadero/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.parcial2_lavadero
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.parcial2_lavadero.ui.theme.Parcial2_LavaderoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Parcial2_LavaderoTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Parcial2_LavaderoTheme {
+        Greeting("Android")
+    }
+}
